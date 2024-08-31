e4e7a04534b801e67862bb68587a24599f1f51c0
jeet1703 <prabhjeetdec03@gmail.com>
2024-08-31 16:00:14 +0530

    IMages

--- src/Components/PlayerSection.jsx
+++ src/Components/PlayerSection.jsx
@@ -6,17 +6,17 @@ const PlayerSection = () => {
     <div className="  py-20 px-6">
       {/* Player Images */}
       <div className="flex flex-col items-center space-y-10">
-        <img src="/player1.png" alt="Player 1" className="w-full max-w-6xl" />
-        <img src="/player2.png" alt="Player 2" className="w-full  max-w-6xl" />
-        <img src="/player3.png" alt="Player 3" className="w-full  max-w-6xl" />
-        <img src="/player4.png" alt="Player 4" className="w-full  max-w-6xl" />
+        <img src="/player1.webp" alt="Player 1" className="w-full max-w-6xl" />
+        <img src="/player2.webp" alt="Player 2" className="w-full  max-w-6xl" />
+        <img src="/player3.webp" alt="Player 3" className="w-full  max-w-6xl" />
+        <img src="/player4.webp" alt="Player 4" className="w-full  max-w-6xl" />
       </div>
 
       {/* Bottom Image */}
      
     </div>
     <div className="flex justify-center mt-10">
-        <img src="/bottom.png" alt="Bottom Image" className="w-full " />
+        <img src="/bottom.webp" alt="Bottom Image" className="w-full " />
       </div>
     </div>
   );
--- src/Components/WozooWorld.jsx
+++ src/Components/WozooWorld.jsx
@@ -6,7 +6,7 @@ const WozooWorld = () => {
       {/* Asteroid Image (Center, behind the buildings) */}
       <div className="absolute inset-0 z-0 flex">
         <img
-          src="/aestroid.png"
+          src="/aestroid.webp"
           alt="Asteroid with Buildings and Blackhole"
           className="w-full h-full absolute object-cover z-20"
         />
@@ -14,21 +14,21 @@ const WozooWorld = () => {
 
       {/* Right Building Image */}
       <img
-        src="/rightbuild.png"
+        src="/rightbuild.webp"
         alt="Right Building"
         className="absolute top-0 right-0 z-10 w-1/4 md:w-1/5 lg:w-1/6 object-contain"
       />
 
       {/* Left Building Image */}
       <img
-        src="/leftbuild.png"
+        src="/leftbuild.webp"
         alt="Left Building"
         className="absolute top-1/2 left-0 transform -translate-y-1/2 z-10 w-1/4 md:w-1/5 lg:w-1/6 object-contain"
       />
 
       {/* Wozoo Logo (Centered in Asteroid) */}
       <div className="relative z-20 flex items-center justify-center pt-96">
-        <img src="/wozoworld.png" alt="Wozoo Logo" className="w-48 md:w-64 lg:w-72" />
+        <img src="/wozoworld.webp" alt="Wozoo Logo" className="w-48 md:w-64 lg:w-72" />
       </div>
 
       {/* Content Below */}
